55206cbf88cfc6bbc3fe633d0c2c20d94dca5620
angelm1974 <mariusz980@gmail.com>
2023-11-25 16:00:27 +0100

    azure 1 commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Moja aplikacja dla Azure static web apps
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
     </div>
   );
